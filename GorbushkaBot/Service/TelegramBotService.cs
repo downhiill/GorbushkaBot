ad45e22f9c7eec8f07aede80f8515bfe5a36244b
downhiill <downh068@gmail.com>
2025-02-22 22:55:03 +0200

    поменял список админов

--- GorbushkaBot/Service/TelegramBotService.cs
+++ GorbushkaBot/Service/TelegramBotService.cs
@@ -26,7 +26,7 @@ public class TelegramBotService
 
 
 
-    long[] adminIds = { 8018159474, 448145168, 388009185, 7069858455 }; // Список ID администраторов
+    long[] adminIds = { 8018159474, 448145168, 7069858455 }; // Список ID администраторов
 
     public TelegramBotService(UserApplicationService userApplicationService, ApplicationService applicationService, UserAcceptService userAcceptService, ApplicationDbContext applicationDbContext)
     {
